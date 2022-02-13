94b141ac0c9693cef0a1c3d42633dcd2ad068b0d
AngelJohank <angeljohank@gmail.com>
2022-02-12 21:51:47 -0500

    reset

--- src/index.js
+++ src/index.js
@@ -3,20 +3,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 // Routes
-import NavBar from './components/NavBar';
-import App from './routes/App';
-import Gif from './routes/Gif';
-import NotFound from './routes/NotFound';
 
 // Render
-ReactDOM.render(
-  <BrowserRouter>
-    <NavBar />
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="gif/:id" element={<Gif />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+ReactDOM.render(<h1>Hello world</h1>, document.getElementById('root'));
--- src/routes/NotFound.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function NotFound() {
-  return <h1>Sorry page not found :(</h1>;
-}
